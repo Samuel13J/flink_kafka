ebc947e858d652764ee1e68645972093d46adbcb
Samuel13J <fjdswj@163.com>
2023-03-30 09:57:34 +0800

    flink消费kafka数据写入mysql

--- src/main/java/org/com/jaywong/log/flink/handler/ExceptionLogHandlerBySql.java
+++ src/main/java/org/com/jaywong/log/flink/handler/ExceptionLogHandlerBySql.java
@@ -1,6 +1,142 @@
-package org.com.jaywong.log.flink.handler;/**
-@author wangjie
-@create 2023-03-28 13:55
-*/
-    public class ExceptionLogHandlerBySql {
+package org.com.jaywong.log.flink.handler;
+
+/**
+ * @author wangjie
+ * @create 2023-03-28 13:55
+ */
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.SqlDialect;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.catalog.hive.HiveCatalog;
+import org.com.jaywong.log.utils.PropertiesUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.com.jaywong.log.utils.PropertiesUtils;
+
+public class ExceptionLogHandlerBySql {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionLogHandlerBySql.class);
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        PropertiesUtils instance = PropertiesUtils.getInstance();
+        //重启策略之固定间隔 (Fixed delay)
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(instance.getFlinkFixedDelayRestartTimes(),
+                Time.of(instance.getFlinkFixedDelayRestartInterval(), TimeUnit.MINUTES)));
+
+        //设置间隔多长时间产生checkpoint
+        env.enableCheckpointing(instance.getFlinkCheckpointsInterval());
+        //设置模式为exactly-once （这是默认值）
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        //确保检查点之间有至少500 ms的间隔【checkpoint最小间隔】
+        // env.getCheckpointConfig().setMinPauseBetweenCheckpoints(instance.getFlinkMinPauseBetweenCheckpoints());
+        //检查点必须在一分钟内完成，或者被丢弃【checkpoint的超时时间】
+        env.getCheckpointConfig().setCheckpointTimeout(instance.getFlinkCheckpointTimeout());
+        //同一时间只允许进行一个检查点
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(instance.getFlinkMaxConcurrentCheckpoints());
+        // 表示一旦Flink处理程序被cancel后，会保留Checkpoint数据，以便根据实际需要恢复到指定的Checkpoint【详细解释见备注】
+        env.getCheckpointConfig()
+                .enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        env.setParallelism(1);
+
+        // flink table
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().inStreamingMode().useBlinkPlanner().build();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
+
+        // 构造 kafka source, 用 DEFAULT
+        tableEnv.getConfig().setSqlDialect(SqlDialect.DEFAULT);
+        String sourceDrop = "drop table if exists kafka_exception";
+        String sourceTable = "CREATE TABLE kafka_exception ("
+                + "  serviceId STRING,"
+                + "  serverName STRING,"
+                + "  serverIp STRING,"
+                + "  title STRING,"
+                + "  operationPath STRING,"
+                + "  url STRING,"
+                + "  stack STRING,"
+                + "  exceptionName STRING,"
+                + "  exceptionInfo STRING,"
+                + "  operationUser STRING,"
+                + "  operationIp STRING,"
+                + "  orgId BIGINT,"
+                + "  methodClass STRING,"
+                + "  fileName STRING,"
+                + "  methodName STRING,"
+                + "  operationData STRING,"
+                + "  occurrenceTime BIGINT"
+                + ") WITH ("
+                + "    'connector' = 'kafka',"
+                + "    'topic' = '" + instance.getExceptionTopic() + "',"
+                + "    'properties.bootstrap.servers' = '" + instance.getKafkServer() + "',"
+                + "    'properties.group.id' = '" + instance.getGroupId() + "',"
+                + "    'scan.startup.mode' = 'earliest-offset',"
+                + "    'format' = 'json',"
+                + "    'json.fail-on-missing-field' = 'false',"
+                + "    'json.ignore-parse-errors' = 'true'"
+                + "  )";
+        System.out.println("=================sourcesql打印开始========================");
+        tableEnv.executeSql(sourceDrop);
+        tableEnv.executeSql(sourceTable);
+        System.out.println(sourceTable);
+        System.out.println("=================sourcesql打印结束========================");
+
+        // 构造 hive catalog(这个可以任意编写)
+        String name = "mycatalog";
+        String defaultDatabase = instance.getDatabase();
+        String hiveConfDir = instance.getHiveConf();
+        HiveCatalog hive = new HiveCatalog(name, defaultDatabase, hiveConfDir);
+        tableEnv.registerCatalog(name, hive);
+        tableEnv.useCatalog(name);
+
+        // hive sink
+        tableEnv.getConfig().setSqlDialect(SqlDialect.HIVE);
+        tableEnv.useDatabase(defaultDatabase);
+        String sinkDrop = "drop table if exists hive_exception";
+        String sinkTable = "CREATE TABLE hive_exception ("
+                + "  service_id STRING,"
+                + "  server_name STRING,"
+                + "  server_ip STRING,"
+                + "  title STRING,"
+                + "  operation_path STRING,"
+                + "  url STRING,"
+                + "  stack STRING,"
+                + "  exception_name STRING,"
+                + "  exception_info STRING,"
+                + "  operation_user STRING,"
+                + "  operation_ip STRING,"
+                + "  org_id BIGINT,"
+                + "  method_class STRING,"
+                + "  file_name STRING,"
+                + "  method_name STRING,"
+                + "  operation_data STRING,"
+                + "  occurrence_time String"
+                + " ) PARTITIONED BY (dt STRING) STORED AS parquet TBLPROPERTIES ("
+                + "     'partition.time-extractor.timestamp-pattern'='$dt 00:00:00',"
+                + "     'sink.partition-commit.trigger'='process-time',"
+                + "     'sink.partition-commit.delay'='0 s',"
+                + "     'sink.partition-commit.policy.kind'='metastore,success-file'"
+                + ")";
+        System.out.println("=================sinksql打印开始========================");
+        tableEnv.executeSql(sinkDrop);
+        tableEnv.executeSql(sinkTable);
+        System.out.println(sinkTable);
+        System.out.println("=================sinksql打印结束========================");
+        String sql = "INSERT INTO TABLE hive_exception"
+                + " SELECT serviceId, serverName, serverIp, title, operationPath, url, stack, exceptionName, exceptionInfo, operationUser, operationIp,"
+                + " orgId, methodClass, fileName, methodName, operationData, from_unixtime(cast(occurrenceTime/1000 as bigint),'yyyy-MM-dd HH:mm:ss'), from_unixtime(cast(occurrenceTime/1000 as bigint),'yyyy-MM-dd')"
+                + " FROM kafka_exception";
+        tableEnv.executeSql(sql);
+    }
 }
--- src/main/java/org/com/jaywong/log/utils/PropertiesUtils.java
+++ src/main/java/org/com/jaywong/log/utils/PropertiesUtils.java
@@ -1,6 +1,177 @@
-package org.com.jaywong.log.utils;/**
-@author wangjie
-@create 2023-03-28 13:51
-*/
-    public class PropertiesUtils {
+package org.com.jaywong.log.utils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @author wangjie
+ * @create 2023-03-28 13:51
+ */
+public class PropertiesUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(PropertiesUtils.class);
+    private static PropertiesUtils instance = null;
+    /** 间隔xxx秒产生checkpoing **/
+    private Integer flinkCheckpointsInterval = null;
+    /** 确保检查点之间有至少xxx ms的间隔 **/
+    private Integer flinkMinPauseBetweenCheckpoints = null;
+    /** 检查点必须在一分钟内完成，或者被丢弃【checkpoint的超时时间】 **/
+    private Integer flinkCheckpointTimeout = null;
+    /** 同一时间只允许进行一个检查点 **/
+    private Integer flinkMaxConcurrentCheckpoints = null;
+    /** 尝试重启次数 **/
+    private Integer flinkFixedDelayRestartTimes = null;
+    /** 每次尝试重启时之间的时间间隔 **/
+    private Integer flinkFixedDelayRestartInterval = null;
+    /** kafka source 的并行度 **/
+    private Integer kafkaSourceParallelism = null;
+    /** hive sink 的并行度 **/
+    private Integer hiveSinkParallelism = null;
+    /** kafka集群 **/
+    private String kafkServer = null;
+    /** 消费者组id **/
+    private String groupId = null;
+    private Boolean enableAutoCommit = null;
+    private Long autoCommitInterval = null;
+    private String keyDeserializer = null;
+    private String valueDeserializer = null;
+    private String exceptionTopic = null;
+    private String logTopic = null;
+    private String hiveConf = null;
+    private String database = null;
+
+    /**
+     * 静态代码块
+     */
+    private PropertiesUtils() {
+        InputStream in = null;
+        try {
+            // 读取配置文件,通过类加载器的方式读取属性文件
+            in = PropertiesUtils.class.getClassLoader().getResourceAsStream("project-config-test.properties");
+//            in = PropertiesUtils.class.getClassLoader().getResourceAsStream("test-win10.properties");
+//            in = PropertiesUtils.class.getClassLoader().getResourceAsStream("test-linux.properties");
+            Properties prop = new Properties();
+            prop.load(in);
+            // flink配置
+            flinkCheckpointsInterval = Integer.parseInt(prop.getProperty("flink.checkpoint.interval").trim());
+            flinkMinPauseBetweenCheckpoints =
+                    Integer.parseInt(prop.getProperty("flink.checkpoint.minPauseBetweenCheckpoints").trim());
+            flinkCheckpointTimeout = Integer.parseInt(prop.getProperty("flink.checkpoint.checkpointTimeout").trim());
+            flinkMaxConcurrentCheckpoints =
+                    Integer.parseInt(prop.getProperty("flink.checkpoint.maxConcurrentCheckpoints").trim());
+            flinkFixedDelayRestartTimes = Integer.parseInt(prop.getProperty("flink.fixedDelayRestart.times").trim());
+            flinkFixedDelayRestartInterval =
+                    Integer.parseInt(prop.getProperty("flink.fixedDelayRestart.interval").trim());
+            kafkaSourceParallelism = Integer.parseInt(prop.getProperty("flink.kafka.source.parallelism").trim());
+            hiveSinkParallelism = Integer.parseInt(prop.getProperty("flink.hive.sink.parallelism").trim());
+
+            // kafka配置
+            kafkServer = prop.getProperty("kafka.bootstrap.servers").trim();
+            groupId = prop.getProperty("kafka.consumer.group.id").trim();
+            enableAutoCommit = Boolean.valueOf(prop.getProperty("kafka.consumer.enableAutoCommit").trim());
+            autoCommitInterval = Long.valueOf(prop.getProperty("kafka.consumer.autoCommitInterval").trim());
+            keyDeserializer = prop.getProperty("kafka.consumer.key.deserializer").trim();
+            valueDeserializer = prop.getProperty("kafka.consumer.value.deserializer").trim();
+            exceptionTopic = prop.getProperty("kafka.exception.topic").trim();
+            logTopic = prop.getProperty("kafka.log.topic").trim();
+
+            hiveConf = prop.getProperty("hive.conf").trim();
+            database = prop.getProperty("hive.database").trim();
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.error("流关闭失败");
+            }
+        }
+    }
+
+    public static PropertiesUtils getInstance() {
+        if (instance == null) {
+            instance = new PropertiesUtils();
+        }
+        return instance;
+    }
+
+    public Integer getFlinkCheckpointsInterval() {
+        return flinkCheckpointsInterval;
+    }
+
+    public Integer getFlinkMinPauseBetweenCheckpoints() {
+        return flinkMinPauseBetweenCheckpoints;
+    }
+
+    public Integer getFlinkCheckpointTimeout() {
+        return flinkCheckpointTimeout;
+    }
+
+    public Integer getFlinkMaxConcurrentCheckpoints() {
+        return flinkMaxConcurrentCheckpoints;
+    }
+
+    public Integer getFlinkFixedDelayRestartTimes() {
+        return flinkFixedDelayRestartTimes;
+    }
+
+    public Integer getFlinkFixedDelayRestartInterval() {
+        return flinkFixedDelayRestartInterval;
+    }
+
+    public Integer getKafkaSourceParallelism() {
+        return kafkaSourceParallelism;
+    }
+
+    public Integer getHiveSinkParallelism() {
+        return hiveSinkParallelism;
+    }
+
+    public String getKafkServer() {
+        return kafkServer;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public Boolean getEnableAutoCommit() {
+        return enableAutoCommit;
+    }
+
+    public Long getAutoCommitInterval() {
+        return autoCommitInterval;
+    }
+
+    public String getKeyDeserializer() {
+        return keyDeserializer;
+    }
+
+    public String getValueDeserializer() {
+        return valueDeserializer;
+    }
+
+    public String getExceptionTopic() {
+        return exceptionTopic;
+    }
+
+    public String getLogTopic() {
+        return logTopic;
+    }
+
+    public String getHiveConf() {
+        return hiveConf;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
 }
