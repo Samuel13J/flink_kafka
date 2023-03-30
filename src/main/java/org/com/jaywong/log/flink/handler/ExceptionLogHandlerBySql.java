2c42714217a1c4e56c3b6817d070dc2c6ddb52ae
Samuel13J <fjdswj@163.com>
2023-03-30 09:55:47 +0800

    flink消费kafka数据写入mysql

---
+++ src/main/java/org/com/jaywong/log/flink/handler/ExceptionLogHandlerBySql.java
@@ -0,0 +1,6 @@
+package org.com.jaywong.log.flink.handler;/**
+@author wangjie
+@create 2023-03-28 13:55
+*/
+    public class ExceptionLogHandlerBySql {
+}
---
+++ src/main/java/org/com/jaywong/log/utils/PropertiesUtils.java
@@ -0,0 +1,6 @@
+package org.com.jaywong.log.utils;/**
+@author wangjie
+@create 2023-03-28 13:51
+*/
+    public class PropertiesUtils {
+}
